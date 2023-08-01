b4e1ce83581e12f6db3fcb38f21f9e72d5a8ac87
snmed <sandro@dallo.biz>
2023-08-01 22:01:08 +0200

    Add more type

--- src-tauri/src/books/models.rs
+++ src-tauri/src/books/models.rs
@@ -1,10 +1,11 @@
 // This file contains all models used for the books module.
 
+// Remove as soon implementation is done
+use chrono::{DateTime, Utc};
 #[allow(dead_code)]
-
 use std::{error::Error, fmt::Display};
 
-use chrono::{DateTime, Utc};
+// All known error for the books module.
 
 #[derive(Debug, PartialEq)]
 pub enum BookError {
@@ -23,13 +24,52 @@ impl Display for BookError {
     }
 }
 
+/// SortOrder defines the direction of a query.
+#[derive(Debug, PartialEq)]
+pub enum SortOrder {
+    Asc,
+    Desc,
+}
+
+impl From<String> for SortOrder {
+    fn from(value: String) -> Self {
+        match value.to_lowercase().as_ref() {
+            "desc" => SortOrder::Desc,
+            _ => SortOrder::Asc,
+        }
+    }
+}
+
+impl From<&str> for SortOrder {
+    fn from(value: &str) -> Self {
+        SortOrder::from(value.to_string())
+    }
+}
+
+/// SortDescriptor describes a column and which sort order to use.
+pub struct SortDescriptor(String, SortOrder);
+
+/// StoreResult a generic store result.
+pub struct StoreResult<T> {
+    total: u64,
+    skipped: u64,
+    items: Vec<T>,
+}
+
+pub struct SearchConfig {
+    skip: Option<u64>,
+    sort: Option<Vec<SortDescriptor>>
+}
+
 /// BookDB provides functions to store and retrieve books from the underlying data store.
 pub trait BookDB {
-    fn add_book(book: &mut Book) -> Result<&mut Book, BookError>;
+    fn add_book(book: Book) -> Result<Book, BookError>;
     fn update_book(book: &mut Book) -> Result<(), BookError>;
     fn delete_book(book: &Book) -> Result<(), BookError>;
     fn delete_book_by_id(id: i64) -> Result<(), BookError>;
-    // TODO: Add more functions to the BookDB trait
+    fn fetch_books(search: &str) -> Result<StoreResult<Book>, BookError>;
+
+    fn get_tags() -> Result<StoreResult<Tag>, BookError>;
 }
 
 /// A book representation for the bookshelf application.
@@ -58,4 +98,35 @@ pub struct Tag {
     id: i64,
     created: DateTime<Utc>,
     updated: DateTime<Utc>,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::books::models::SortOrder;
+
+    // This test is only to get familiar with Rust testing
+    #[test]
+    fn test_sort_order_from() {
+        assert_eq!(SortOrder::from("any Value"), SortOrder::Asc);
+        assert_eq!(SortOrder::from("asc"), SortOrder::Asc);
+
+        let s = "deSC".to_string();
+        assert_eq!(SortOrder::from(s), SortOrder::Desc);
+
+        assert_eq!(SortOrder::from("desc"), SortOrder::Desc);
+        assert_eq!(SortOrder::from("dEsC"), SortOrder::Desc);
+    }
+
+    #[test]
+    fn my_test() {
+
+        let myfn = |s: String| println!("intern function {}!", s);
+
+        let txt = String::from("John");
+
+        myfn(txt.clone());
+        println!("OUTSIDE {}", txt);
+
+        
+    }
 }
