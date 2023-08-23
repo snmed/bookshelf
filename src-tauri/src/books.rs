51d6c8bb4bff84a55ca330c4edd99dbf0baeafcd
snmed <sandro@dallo.biz>
2023-08-23 20:47:49 +0200

    Add book pool

--- src-tauri/src/books.rs
+++ src-tauri/src/books.rs
@@ -1,36 +1,74 @@
 use std::collections::HashMap;
-use std::io;
+
 
 use self::models::{BookDB, BookError};
-use crate::pool::PoolManager;
-use crate::{macros, from_err};
+use crate::pool::{PoolItem, PoolManager};
+use crate::{from_err};
 
 // Module declarations
 pub mod models;
 mod store;
 
-
-
 #[derive(Debug)]
 pub enum Error {
     PoolAlreadyAdded,
-    BookError(BookError)
+    PoolNotFound,
+    CurrentPoolNotSet,
+    BookError(BookError),
 }
-
 from_err!(Error, BookError, BookError);
 
 pub type Result<T = (), E = Error> = std::result::Result<T, E>;
 
 pub type BookPool = PoolManager<Box<dyn BookDB>>;
 
-
 pub struct BookManager {
     book_db_pools: HashMap<String, BookPool>,
-    current: String
+    current: Option<String>,
 }
 
 impl BookManager {
     pub fn add_pool<K: AsRef<str>>(&mut self, pool_name: K, pool: BookPool) -> Result {
         todo!()
+    }
+
+    pub fn remove_pool<T: AsRef<str>>(&mut self, pool_name: T) -> Option<BookPool> {
+        match self.book_db_pools.remove_entry(pool_name.as_ref()) {
+            Some(entry) => {
+                if self.current.is_some() && entry.0.as_str() == pool_name.as_ref() {
+                    self.current = None;
+                }
+                Some(entry.1)
+            }
+            None => None,
+        }
+    }
+
+    pub fn is_current_pool_set(&self) -> bool {
+        self.current.is_some()
+    }
+
+    pub fn set_current_pool<T: AsRef<str>>(&mut self, pool_name: T) -> Result {
+        if self.book_db_pools.contains_key(pool_name.as_ref()) {
+            self.current.replace(pool_name.as_ref().to_string());
+            return Ok(());
+        }
+
+        Err(Error::PoolNotFound)
+    }
+
+    pub fn get_pools(&self) -> Vec<&str> {
+        self.book_db_pools.keys().map(|k| k.as_str()).collect()
+    }
+
+    pub fn get_current_pool(&self) -> Result<PoolItem<Box<dyn BookDB>>> {
+        match self.current.as_ref() {
+            Some(s) => Ok(self
+                .book_db_pools
+                .get(s)
+                .ok_or(Error::PoolNotFound)?
+                .get_pool_item()),
+            None => Err(Error::CurrentPoolNotSet),
+        }
     }
 }
